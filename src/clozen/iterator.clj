5b605d7e86568114fb442d6a6f40788d738d2be5
Zenna Tavares <zennatavares@gmail.com>
2014-03-05 11:45:27 -0500

    renamed iterator to itr and added start of subtreeleavesfirstitr

--- src/clozen/iterator.clj
+++ src/clozen/iterator.clj
@@ -5,7 +5,7 @@
   (:require [clojure.zip :as zip]))
 
 ;; Iterators
-(defprotocol Iterator
+(defprotocol Itr
   "This iterator is a purely functional version of an iterator ala C++
    The purpose is to abstract away the mechanics of iteration for better
    modularity.
@@ -17,71 +17,102 @@
    Since the iterator contains lots more information than you may want for
    any particular algorithm that uses it, we must use the realise function to
    get the actual value"
-  (step [iterator])
-  (end? [iterator])
-  (update [iterator node])
-  (root [iterator])
-  (realise [iterator]))
+  (step [itr])
+  (end? [itr])
+  (update [itr node])
+  (root [itr])
+  (realise [itr]))
 
-(defrecord NodeIterator
-  ^{:doc
-    "Iterates through all nodes of an expression depth-first.
-     e.g. (+ 3 4 (rand 0 1) (rand 3 4)) ->
-          (+ 3 4 (rand 0 1) (rand 3 4)), +, 3, (rand 0 1), rand, 0, .."}
-  [tree zipped-tree])
-
-(defrecord SubtreeIterator
+; There's more than one way to iterate through a collection
+; - e.g. left->right vs right->left
+; And there's more than one type of information that could be iterated over
+; - e.g. nodes vs subtrees
+; The design question is whether in each of these dimensions, different types
+; should be distinguished by methods, or types
+; Option one, both of these dimensions should be split on type, e.g. then I
+; (defrecord NodeIterator) (defrecord SubtreeIterator)
+; (defrecord SubtreeIterator-leaves-first)
+; and the good news is that we can always just use step
+; Bad news is that 1. I might end up with long iterator names
+; If in some use case I need to go right until something and then do something else - I 
+(defrecord SubtreeLeavesFirstItr
   ^{:doc
     "Iterates through all subtrees of an expression depth-first.
     e.g. (+ 3 4 (rand 0 1) (rand 3 4)) ->
          (+ 3 4 (rand 0 1) (rand 3 4)), (rand 0 1), (rand 3 4)"}
   [tree zipped-tree])
 
-(defn node-iterator
+(defrecord NodeItr
+  ^{:doc
+    "Iterates through all nodes of an expression depth-first.
+     e.g. (+ 3 4 (rand 0 1) (rand 3 4)) ->
+          (+ 3 4 (rand 0 1) (rand 3 4)), +, 3, (rand 0 1), rand, 0, .."}
+  [tree zipped-tree])
+
+(defrecord SubtreeItr
+  ^{:doc
+    "Iterates through all subtrees of an expression depth-first.
+    e.g. (+ 3 4 (rand 0 1) (rand 3 4)) ->
+         (+ 3 4 (rand 0 1) (rand 3 4)), (rand 0 1), (rand 3 4)"}
+  [tree zipped-tree])
+
+(defn node-itr
   "Node iterator factory"
   [exp]
-  (NodeIterator. exp (zip/zipper coll? seq (fn [_ c] c) exp)))
+  (NodeItr. exp (zip/zipper coll? seq (fn [_ c] c) exp)))
 
-(defn subtree-iterator
+(defn subtree-itr
   "Subtree iterator factory"
   [exp]
-  (SubtreeIterator. exp (zip/zipper coll? seq (fn [_ c] c) exp)))
+  (SubtreeItr. exp (zip/zipper coll? seq (fn [_ c] c) exp)))
 
-(def abstract-zip-iterator-impl
+(def abstract-zip-itr-impl
   "Abstract implementation for iterators built upon clojure/zip"
   {:step
-    (fn [iterator]
-      (assoc iterator :zipped-tree (zip/next (.zipped-tree iterator))))
+    (fn [itr]
+      (assoc itr :zipped-tree (zip/next (.zipped-tree itr))))
 
    :end?
-   (fn [iterator]
-     (zip/end? (.zipped-tree iterator)))
+   (fn [itr]
+     (zip/end? (.zipped-tree itr)))
 
    :update
-   (fn [iterator node]
-     (assoc iterator :zipped-tree (zip/replace (.zipped-tree iterator) node)))
+   (fn [itr node]
+     (assoc itr :zipped-tree (zip/replace (.zipped-tree itr) node)))
 
    :root
-   (fn [iterator]
-     (zip/root (.zipped-tree iterator)))
+   (fn [itr]
+     (zip/root (.zipped-tree itr)))
 
    :realise
-   (fn [iterator]
-     (first (.zipped-tree iterator)))})
+   (fn [itr]
+     (first (.zipped-tree itr)))})
 
 ; Node iterator is the same as the abstract iterator
-(extend NodeIterator
-  Iterator
-  abstract-zip-iterator-impl)
+(extend NodeItr
+  Itr
+  abstract-zip-itr-impl)
 
-(extend SubtreeIterator
-  Iterator
-  (assoc abstract-zip-iterator-impl
+(extend SubtreeItr
+  Itr
+  (assoc abstract-zip-itr-impl
     ; Keep doing zip/next until I get to a branch
     :step
-    (fn [iterator]
+    (fn [itr]
       (let [zip-tree
             (repeat-until zip/next
-                          (.zipped-tree iterator)
+                          (.zipped-tree itr)
                           #(or (zip/end? %) (zip/branch? %)))]
-        (assoc iterator :zipped-tree zip-tree)))))
+        (assoc itr :zipped-tree zip-tree)))))
+
+(extend SubtreeLeavesFirstItr
+  Itr
+  (assoc abstract-zip-itr-impl
+    ; Keep doing zip/next until I get to a branch
+    :step
+    (fn [itr]
+      (let [zip-tree
+            (repeat-until zip/next
+                          (.zipped-tree itr)
+                          #(or (zip/end? %) (zip/branch? %)))]
+        (assoc itr :zipped-tree zip-tree)))))
