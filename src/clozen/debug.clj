dcd46d2d86cab279624e66a30e88027886c85158
Zenna Tavares <zennatavares@gmail.com>
2014-03-11 21:55:37 -0400

    add iterator constraints

--- src/clozen/debug.clj
+++ src/clozen/debug.clj
@@ -2,4 +2,6 @@
       :author "Zenna Tavares"}
   clozen.debug)
 
-(defmacro dbg[x] `(let [x# ~x] (println "dbg:" '~x "=" x#) x#))
+(defmacro dbg
+  ([x] `(let [x# ~x] (println "dbg:" '~x "=" x#) x#))
+  ([f x] `(let [x# ~x] (println "dbg-f:" '~x "=" (~f x#)) x#)))
--- src/clozen/iterator.clj
+++ src/clozen/iterator.clj
@@ -1,7 +1,8 @@
 (ns ^{:doc "Iterators"
       :author "Zenna Tavares"}
   clozen.iterator
-  (:require [clozen.helpers :refer [repeat-until]])
+  (:require [clozen.helpers :refer [repeat-until repeat-before-until]])
+  (:require [clozen.debug :refer [dbg]])
   (:require [clojure.zip :as zip]))
 
 ;; Iterators
@@ -32,14 +33,17 @@
 ; Option one, both of these dimensions should be split on type, e.g. then I
 ; (defrecord NodeIterator) (defrecord SubtreeIterator)
 ; (defrecord SubtreeIterator-leaves-first)
-; and the good news is that we can always just use step
+; and the good news is that we can always just use (step itr)
 ; Bad news is that 1. I might end up with long iterator names
-; If in some use case I need to go right until something and then do something else - I 
+; 2. If some algorithm requires two types of iteration, I'll need two iterators.
+; Option 2: is have multiple protocols.
+; I choose Option 1 as it is simpler 
 (defrecord SubtreeLeavesFirstItr
   ^{:doc
-    "Iterates through all subtrees of an expression depth-first.
-    e.g. (+ 3 4 (rand 0 1) (rand 3 4)) ->
-         (+ 3 4 (rand 0 1) (rand 3 4)), (rand 0 1), (rand 3 4)"}
+    "Iterates through all subtrees of an expression under constraint that
+     no subtree will be seen before its children subtrees have been seen.
+     e.g. (+ 3 4 (rand 0 1) (rand 3 4)) ->
+          (+ 3 4 (rand 0 1) (rand 3 4)), (rand 0 1), (rand 3 4)"}
   [tree zipped-tree])
 
 (defrecord NodeItr
@@ -106,6 +110,8 @@
         (assoc itr :zipped-tree zip-tree)))))
 
 (defn go-to-next
+  "For a leaves first subtree traversal, go to the next subtree.
+   This is the parent of the next rightmost leaf"
   [zipped-tree]
   (loop [zipped-tree zipped-tree]
     (cond
@@ -121,18 +127,19 @@
 (extend SubtreeLeavesFirstItr
   Itr
   (assoc abstract-zip-itr-impl
-    ; Keep doing zip/next until I get to a branch
-    :end?
-    (fn [itr]
-      (nil? ((.zipped-tree itr) 1)))
-
     :step
     (fn [itr]
       (let [zipped-tree (.zipped-tree itr)
             zip-tree-moved
-            (if (nil? (zip/right zipped-tree))
-                (zip/up zipped-tree)
-                (go-to-next (zip/right zipped-tree)))]
+            (cond
+              (nil? (zip/up zipped-tree))
+              (assoc zipped-tree 1 :end)
+              
+              (nil? (zip/right zipped-tree))
+              (zip/up zipped-tree)
+
+              :else
+              (go-to-next (zip/right zipped-tree)))]
         (assoc itr :zipped-tree zip-tree-moved)))))
 
 (defn subtree-leaves-first-itr
@@ -141,6 +148,20 @@
   (SubtreeLeavesFirstItr. exp 
     (-> (zip/zipper coll? seq (fn [_ c] c) exp) zip/down go-to-next)))
 
+(defn add-itr-constraint
+  "Adds a constraint to the iterator
+   Note it this is a predicate on the iterator, the actual value can be
+   got at by using realise, e.g. for a seq constraint #(seq? (realise %))"
+  [itr pred?]
+  (reify Itr
+    (end? [_] (end? itr))
+    (update [_ node] (update itr node))
+    (root [_] (root itr))
+    (realise [_] (realise itr))
+    (step [_] (add-itr-constraint
+                (repeat-until step itr #(or (end? %) (pred? %)))
+                pred?))))
+
 (defn iterate-and-print
   [itr]
   (loop [itr itr]
@@ -148,6 +169,20 @@
       (println (realise itr))
       (recur (step itr)))))
 
-(comment  
-  (def x (subtree-leaves-first-itr '(+ 1 2 (* 3 4) x y (/ (plus a b) 6))))
-  (iterate-and-print x))
+(defn iterate-and-print-fn
+  [itr f]
+  (loop [itr itr]
+    (when (not (end? itr))
+      (println (realise itr) "-" (f itr))
+      (recur (step itr)))))
+
+(comment
+  (def x (node-itr '(+ 1 2 (* 3 4) (+ 4 5 6) y (/ (plus a b) 6))))
+  (def x (node-itr '(+ 1 2 3 (* 3 4))))
+  (def y (add-itr-constraint x #(seq? (dbg (realise %)))))  
+  (def z (add-itr-constraint y #(= (dbg (count (realise %))) 30)));#(= (count %) 2)))
+  (iterate-and-print (step z))
+
+  (def x (node-itr '(+ 3 2 (if (> x 3) 7 (if false 3 (+ 1 1))))))
+
+  (iterate-and-print-fn x #(check-if (.zipped-tree (dbg realise %)))))
